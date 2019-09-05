505c3e9ba8f73b7367ff4af1d1311840c5075ec1
Jielong <jay_zhong@outlook.com>
2019-09-05 09:12:58 +0800

    code changes

--- unet3d/unet3d_components.py
+++ unet3d/unet3d_components.py
@@ -6,20 +6,29 @@ from torch.nn import init
 import torch.nn.functional as F
 
 
-def weight_init(in_channels, out_channels, k_size):
+def weight_init(in_channels, out_channels, k_size, cuda_available=True):
     weight = torch.empty(size=(out_channels, in_channels, k_size, k_size, k_size))
     weight = init.kaiming_normal_(tensor=weight, mode="fan_in", nonlinearity="leaky_relu")
-    return weight
+    if cuda_available:
+        return weight.cuda()
+    else:
+        return weight
 
 
-def bias_init(out_channels, val):
+def bias_init(out_channels, val, cuda_available=True):
     b = torch.empty(out_channels)
-    return init.constant_(tensor=b, val=val)
+    if cuda_available:
+        return init.constant_(tensor=b, val=val).cuda()
+    else:
+        return init.constant_(tensor=b, val=val)
 
 
-def conv3d_block(x, w, b, stride=1, padding=1, dropout_rate=0.3, training=True):
+def conv3d_block(x, w, b, stride=1, padding=1, dropout_rate=0.3, training=True, cuda_available=True):
     x = F.conv3d(input=x, weight=w, bias=b, stride=stride, padding=padding)
-    x = torch.nn.BatchNorm3d(x.shape[1])(x)
+    if cuda_available:
+        x = torch.nn.BatchNorm3d(x.shape[1]).cuda()(x)
+    else:
+        x = torch.nn.BatchNorm3d(x.shape[1])(x)
     x = F.leaky_relu(x)
     return F.dropout3d(input=x, p=dropout_rate, training=training)
 
@@ -34,7 +43,8 @@ def conv3d_transpose(x, w, b, stride=2, padding=0, output_padding=1):
 
 
 def analysis_path(x, model_depth=4, root_feature_map_channels=16, w_k_size=3, pool_k_size=2, conv_stride=1,
-                  pool_stride=2, conv_padding=1, pool_padding=0, dropout_rate=0.3, training=True):
+                  pool_stride=2, conv_padding=1, pool_padding=0, dropout_rate=0.3, training=True,
+                  cuda_available=True):
     # each analysis layer contains two convolution blocks, this should not be changed
     num_conv_blocks = 2
     # create dicts to store weight, bias and conv ops (include pooling op) for each conv block
@@ -53,13 +63,16 @@ def analysis_path(x, model_depth=4, root_feature_map_channels=16, w_k_size=3, po
                 in_channels = conv_ops["conv_{}_{}".format(depth - 1, 1)].size()[1]
             weights["weight_{}_{}".format(depth, i)] = weight_init(in_channels=in_channels,
                                                                    out_channels=feature_map_channels,
-                                                                   k_size=w_k_size)
-            bias["bias_{}_{}".format(depth, i)] = bias_init(out_channels=feature_map_channels, val=0.5)
+                                                                   k_size=w_k_size,
+                                                                   cuda_available=cuda_available)
+            bias["bias_{}_{}".format(depth, i)] = bias_init(out_channels=feature_map_channels, val=0.5,
+                                                            cuda_available=cuda_available)
             conv_ops["conv_{}_{}".format(depth, i)] = conv3d_block(x=conv,
                                                                    w=weights["weight_{}_{}".format(depth, i)],
                                                                    b=bias["bias_{}_{}".format(depth, i)],
                                                                    stride=conv_stride, padding=conv_padding,
-                                                                   dropout_rate=dropout_rate, training=training)
+                                                                   dropout_rate=dropout_rate, training=training,
+                                                                   cuda_available=cuda_available)
 
             print(conv_ops["conv_{}_{}".format(depth, i)].shape)
         if depth < model_depth - 1:
@@ -73,7 +86,7 @@ def analysis_path(x, model_depth=4, root_feature_map_channels=16, w_k_size=3, po
     return conv_ops, weights, bias
 
 
-def synthesis_path(conv_ops, model_depth, root_feature_map_channels=16):
+def synthesis_path(conv_ops, model_depth=4, root_feature_map_channels=16, cuda_available=True):
     # create dicts to store weight, bias and conv ops (include pooling op) for each conv block
     weights = dict()
     bias = dict()
@@ -86,9 +99,9 @@ def synthesis_path(conv_ops, model_depth, root_feature_map_channels=16):
         # convolution transpose step
         weights["deconv_weight_{}".format(depth)] = weight_init(in_channels=conv_ops["conv_{}_{}".format(depth + 1, 1)].shape[1],
                                                                 out_channels=conv_ops["conv_{}_{}".format(depth + 1, 1)].shape[1],
-                                                                k_size=3)
+                                                                k_size=3, cuda_available=cuda_available)
         bias["deconv_bias_{}".format(depth)] = bias_init(out_channels=conv_ops["conv_{}_{}".format(depth + 1, 1)].shape[1],
-                                                         val=0.5)
+                                                         val=0.5, cuda_available=cuda_available)
         conv_ops_up["conv_up_{}".format(depth)] = conv3d_transpose(x=conv_ops["conv_{}_{}".format(depth + 1, 1)],
                                                                    w=weights["deconv_weight_{}".format(depth)],
                                                                    b=bias["deconv_bias_{}".format(depth)],
@@ -102,26 +115,29 @@ def synthesis_path(conv_ops, model_depth, root_feature_map_channels=16):
         for i in range(num_conv_blocks):
             weights["weight_{}_{}".format(depth, i)] = weight_init(in_channels=conv_ops_up["concat_{}".format(depth)].shape[1],
                                                                    out_channels=feature_map_channels,
-                                                                   k_size=3)
-            bias["bias_{}_{}".format(depth, i)] = bias_init(out_channels=feature_map_channels, val=0.5)
+                                                                   k_size=3,
+                                                                   cuda_available=cuda_available)
+            bias["bias_{}_{}".format(depth, i)] = bias_init(out_channels=feature_map_channels, val=0.5,
+                                                            cuda_available=cuda_available)
             conv_ops_up["conv_{}_{}".format(depth, i)] = conv3d_block(x=conv_ops_up["concat_{}".format(depth)],
                                                                       w=weights["weight_{}_{}".format(depth, i)],
-                                                                      b=bias["bias_{}_{}".format(depth, i)])
+                                                                      b=bias["bias_{}_{}".format(depth, i)],
+                                                                      cuda_available=cuda_available)
             print("hello: ", conv_ops_up["conv_{}_{}".format(depth, i)].shape)
     return conv_ops_up, weights, bias
 
 
-def final_output(conv_ops_up):
+def final_output(conv_ops_up, cuda_available=True):
     weights, bias = dict(), dict()
-    weights["final"] = weight_init(out_channels=1, in_channels=32, k_size=3)
-    bias["final"] = bias_init(1, val=0.1)
+    weights["final"] = weight_init(out_channels=1, in_channels=32, k_size=3, cuda_available=cuda_available)
+    bias["final"] = bias_init(1, val=0.1, cuda_available=cuda_available)
     output = conv3d_block(x=conv_ops_up["conv_{}_{}".format(0, 1)],
                           w=weights["final"],
                           b=bias["final"],
                           stride=1,
                           padding=1,
                           dropout_rate=0.5,
-                          training=True)
+                          training=True, cuda_available=cuda_available)
     print("The shape of final output: ", output.shape)
     return output
 
@@ -135,9 +151,9 @@ if __name__ == "__main__":
     b1 = bias_init(out_channels=16, val=0.5)
     inputs = torch.randn(1, 1, 96, 96, 96)
 
-    conv_results, analysis_weight_dict, analysis_bias_dict = analysis_path(inputs, model_depth=4)
+    conv_results, analysis_weight_dict, analysis_bias_dict = analysis_path(inputs, model_depth=4, cuda_available=False)
     print(conv_results.keys())
     print("\n")
-    up_conv_results, w_dict, b_dict = synthesis_path(conv_results, model_depth=4)
+    up_conv_results, w_dict, b_dict = synthesis_path(conv_results, model_depth=4, cuda_available=False)
     print(up_conv_results.keys())
-    final_output(up_conv_results)
+    final_output(up_conv_results, cuda_available=False)
